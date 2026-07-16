a7737e129ad2c1fac77fc4eea98c9b6d5f38bba4
Evan Greenberg <ev.ben.green@gmail.com>
2026-07-15 17:14:42 -0700

    Missing background updates

--- isofit/inversion/inverse_analytical.py
+++ isofit/inversion/inverse_analytical.py
@@ -184,18 +184,26 @@ def invert_analytical(
         if isinstance(geom.bg_rfl, np.ndarray)
         else rho_dif_dir
     )
+    geom.bg_rfl = rho_dif_dif
 
     # Get all the atmosphere quantities
-    (r, L_tot, L_dir_dir, L_dif_dir, L_dir_dif, L_dif_dif) = (
+    r, L_tot, L_dir_dir, L_dif_dir, L_dir_dif, L_dif_dif = (
         fm.calc_atmosphere_quantities(x_atmosphere, geom, rho_dif_dif=rho_dif_dif)
     )
 
     # Path radiance and spherical albedo
     L_atm = fm.atmosphere.get_L_atm(x_atmosphere, geom)
-    s = r["sphalb"]
+    s_alb = r["sphalb"]
 
-    # Background conditions equal to the superpixel reflectance
-    bg = s * rho_dif_dir
+    # Estimation of background radiance (background terms assumed rho_dir_dif ~= rho_dif_dif)
+    L_bg = fm.calc_rdn_bgrfl(
+        rho_dir_dif=rho_dif_dif,
+        rho_dif_dif=rho_dif_dif,
+        L_dir_dif=L_dir_dif,
+        L_dif_dif=L_dif_dif,
+        L_tot=L_tot,
+        s_alb=s_alb,
+    )
 
     # Get superpixel EOF shift if used
     eof_offset = fm.eof_offset(sub_instrument)
@@ -209,9 +217,9 @@ def invert_analytical(
 
     # The H matrix does not change as a function of x-vector
     H = fm.surface.analytical_model(
-        bg,
         L_tot=L_tot,
         geom=geom,
+        s_alb=s_alb,
         L_dir_dir=L_dir_dir,
         L_dir_dif=L_dir_dif,
         L_dif_dir=L_dif_dir,
@@ -227,7 +235,7 @@ def invert_analytical(
     bounds = np.array(fm.surface.bounds)
 
     # Set inversion target
-    y = meas[winidx] - L_atm[winidx] - eof_offset[winidx]
+    y = meas[winidx] - L_atm[winidx] - eof_offset[winidx] - L_bg[winidx]
     for n in range(num_iter):
         # Measurement uncertainty
         Seps = fm.Seps(x, meas, geom)[winidx, :][:, winidx]
